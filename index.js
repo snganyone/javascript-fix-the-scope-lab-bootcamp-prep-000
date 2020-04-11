3aa95e9cb6dbc5abe61571f93b9dff2f7f606062
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-11 19:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function outer() {
-  return function inner() {
+var funkyFunction = function() {
+  return function(){
     return "FUNKY!"
   }
 }
